ccff38c2b130c85f5f0cac4322c9c7d8ddeaebd2
bala-amypo <bala@amypo.in>
2025-12-18 17:37:02 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/TransactionLogRepository.java
@@ -0,0 +1,5 @@
+package com.example.demo.repository;
+
+public interface TransactionLogRepository extends JpaRepository<TransactionLog,Long>{
+    
+}
--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,3 +1,6 @@
 package com.example.demo.repository;
 
-public interface UserRepository extends JpaRepository<User,
+
+public interface UserRepository extends JpaRepository<User,Long>{
+
+}
