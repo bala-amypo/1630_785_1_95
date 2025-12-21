21cd34ef213850ecf3b958a2a1c0da1bfd7d373a
bala-amypo <bala@amypo.in>
2025-12-21 12:32:47 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/BudgetSummaryServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/BudgetSummaryServiceImpl.java
@@ -3,34 +3,34 @@ package com.example.demo.service.impl;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;   
-import com.example.demo.model.Category;
-import com.example.demo.repository.CategoryRepository;
+import com.example.demo.model.BudgetSummary;
+import com.example.demo.repository.BudgetSummaryRepository;
 import org.springframework.web.bind.annotation.PathVariable;
 import com.example.demo.service.BudgetSummaryService;                
 
 @Service
-public class CategoryServiceImpl implements CategoryService{
+public class BudgetSummaryServiceImpl implements BudgetSummaryService{
 
-    @Autowired CategoryRepository used;
+    @Autowired BudgetSummaryRepository used;
     @Override
-    public Category postData3(Category use){
+    public BudgetSummary postData4(BudgetSummary use){
         return used.save(use);  
     }
     @Override
-    public List<Category>getAllData3(){
+    public List<BudgetSummary>getAllData4(){
         return used.findAll();
     }
     @Override
-    public String DeleteData3(Long id){
+    public String DeleteData4(Long id){
         used.deleteById(id);
         return "Deleted successfully";
     }
     @Override
-    public Category getData3(Long id){
+    public BudgetSummary getData4(Long id){
     return used.findById(id).orElse(null);
     }
     @Override
-    public Category updateData3(Long id,Category entity){
+    public BudgetSummary updateData4(Long id,BudgetSummary entity){
         if(used.existsById(id)){
             entity.setId(id);
             return used.save(entity);
