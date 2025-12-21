8e95675f0bf6005bc4264c883e3ad971042eca78
bala-amypo <bala@amypo.in>
2025-12-21 06:34:08 +0000

    User commit

--- src/main/java/com/example/demo/service/CategoryService.java
+++ src/main/java/com/example/demo/service/CategoryService.java
@@ -3,10 +3,10 @@ package com.example.demo.service;
 import java.util.List;
 import com.example.demo.model.Category;
 
-public interface TransactionService{
+public interface CategoryService{
     Category postData2(Category use);
     List<Category>getAllData2();
     String  DeleteData2(Long id);
     Category getData2(Long id);         
-    Category updateData2(Long id,TransactionLog entity);                                                        
+    Category updateData2(Long id,Category entity);                                                        
 }
