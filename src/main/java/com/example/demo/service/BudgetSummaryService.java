dac44c9964b2774e30b184439464c4406863472f
bala-amypo <bala@amypo.in>
2025-12-21 12:26:48 +0000

    User commit

--- src/main/java/com/example/demo/service/BudgetSummaryService.java
+++ src/main/java/com/example/demo/service/BudgetSummaryService.java
@@ -4,9 +4,9 @@ import java.util.List;
 import com.example.demo.model.BudgetSummary;
 
 public interface BudgetSummaryService{
-    BudgetSummary postData3(BudgetSummary use);
-    List<BudgetSummary>getAllData3();
-    String  DeleteData3(Long id);
+    BudgetSummary postData4(BudgetSummary use);
+    List<BudgetSummary>getAllData4();
+    String  DeleteData4(Long id);
     BudgetSummary getData3(Long id);         
-    Category updateData3(Long id,Category entity);                                                        
+    BudgetSummary updateData3(Long id,BudgetSummary entity);                                                        
 }
