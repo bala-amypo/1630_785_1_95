ce0accf031ed428717ce777033f4b2fe4061aa76
bala-amypo <bala@amypo.in>
2025-12-21 12:44:48 +0000

    User commit

--- src/main/java/com/example/demo/controller/BudgetSummaryController.java
+++ src/main/java/com/example/demo/controller/BudgetSummaryController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import com.example.demo.model.BudgetSummary;
-import com.example.demo.service.BudgetSummary;
+import com.example.demo.service.BudgetSummaryService;
 import jakarta.validation.Valid;
 
 @RequestMapping("/BudgetSummary")
