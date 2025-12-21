5ce03477e039db47d3d506ea5e2d7b75f1e9f453
bala-amypo <bala@amypo.in>
2025-12-21 06:27:50 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/CategoryService.java
@@ -0,0 +1,12 @@
+package com.example.demo.service;
+
+import java.util.List;
+import com.example.demo.model.TransactionLog;
+
+public interface TransactionService{
+    TransactionLog postData2(TransactionLog use);
+    List<TransactionLog>getAllData2();
+    String  DeleteData2(Long id);
+    TransactionLog getData2(Long id);         
+    TransactionLog updateData2(Long id,TransactionLog entity);                                                        
+}
